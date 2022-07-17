dd304facb774a1af6f0847259f133ea43764946b
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-07-17 01:43:09 +0100

    add ANSWER

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
-    $(".question1")
+    $("button.question1").click(function(){
+        $("ul.QUESTION").append("<li>What is your name ?</li>")
+        $("ul.ANSWER").append("<li> Bolaji Enilolobo</li>")
+        $('li').css('background-color', 'green');
+        // $(this).remove();
+    
+    })
+    $("button.question2").click(function(){
+        $("ul.QUESTION").append("<li>How old are you ?</li>")
+        $("ul.ANSWER").append("<li> I am 23years old</li>")
+        // $(this).remove();
+    
+    })
 
+    $("button.question3").click(function(){
+        $("ul.QUESTION").before("<li>What is your qualification ?</li>")
+        $("ul.ANSWER").before("<li> i have N.C.E in Biology</li>")
+    
+    })
+
+    $("button.question4").click(function(){
+        $("ul.QUESTION").prepend("<li>What country are you from ?</li>")
+        $("ul.ANSWER").prepend("<li> I'm from Nigeria</li>")
+    
+    })
+
+    $("button.question5").click(function(){
+        $("ul.QUESTION").after("<li>What is your status ?</li>")
+        $("ul.ANSWER").after("<li>Single</li>")
+    
+    })
+
+   
 })
